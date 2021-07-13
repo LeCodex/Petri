156201ad07826ee13ab9c5aa7f555afa8212e20e
Glitch (petri-game) <none>
2021-07-13 10:28:25 +0000

    🧫🕹 Uploaded from Glitch

---
+++ public/globals.js
@@ -0,0 +1,4 @@
+const PLAYER_COLORS = ["#FF3333", "#3388FF", "#33FF33", "#FFFF33", "#B366FF", "#FF8000"];
+const NUMBER_EMOJIS = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+
+export default {PLAYER_COLORS, NUMBER_EMOJIS}
